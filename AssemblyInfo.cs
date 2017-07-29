52c2c50bd963495a9833190cce75592408511953
Elad Karako <eladkarako@users.noreply.github.com>
2017-07-29 03:21:41 +0300

    initial

---
+++ AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿// Assembly SamFirm, Version 1.0.0.0
+
+[assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
+[assembly: System.Runtime.CompilerServices.CompilationRelaxations(8)]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
+[assembly: System.Runtime.CompilerServices.Extension]
+[assembly: System.Reflection.AssemblyProduct("SamFirm")]
+[assembly: System.Reflection.AssemblyFileVersion("0.3.6.0")]
+[assembly: System.Reflection.AssemblyDescription("")]
+[assembly: System.Reflection.AssemblyConfiguration("")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright zxz0O0 \x00a9  2015")]
+[assembly: System.Reflection.AssemblyTitle("SamFirm")]
+[assembly: System.Reflection.AssemblyTrademark("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("f64041a3-90f5-4bee-a430-1c0f1d604eee")]
+
